ef8101068d141b56ab3d70be489c95b0413f1c93
5arSanti <santiari05@hotmail.com>
2024-11-19 22:29:36 -0500

    Creacion del componente con propiedades

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,4 +1,5 @@
 import { ComponenteBasico } from "../Components/ComponenteBasico";
+import { ComponenteConPropiedades } from "../Components/ComponenteConPropiedades";
 
 import "./App.css";
 
@@ -7,6 +8,15 @@ const App = () => {
         <div>
             <p>Hola, esta es mi primera pagina web con React.js</p>
             <ComponenteBasico/>
+
+            <ComponenteConPropiedades 
+                nombre={"Pepito Perez"} 
+                correo={"correo1.propiedades@email.com"}
+            />
+            <ComponenteConPropiedades 
+                nombre={"Carlos Patiño"} 
+                correo={"correo2.propiedades@email.com"}
+            />
         </div>
     )
 }
