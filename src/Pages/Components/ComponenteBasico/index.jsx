ee3b03bfe6f2dbdd993a59747180888bf2bfe8bb
5arSanti <santiari05@hotmail.com>
2024-11-19 21:58:22 -0500

    New basic component

---
+++ src/Pages/Components/ComponenteBasico/index.jsx
@@ -0,0 +1,16 @@
+const ComponenteBasico = () => {
+    return(
+        <div>
+            <h1>Este es un componente basico</h1>
+
+            <p>React, se puede utilizar como una fucion de HTML, CSS y JavaScript</p>
+            <p>Cada componente es una funcion de tipo flecha, que retorna leguaje JSX (Muy similar al HTML)</p>
+
+            <p>Los componentes son secciones de codigo que se pueden reutilizar las veces que un proyecto requiera</p>
+            <p>Gracias a ellos, el concepto de modularizacion se trabaja a la perfeccion</p>
+            <p>Ya que se pueden crear componentes para tareas especificas</p>
+        </div>
+    );
+}
+
+export { ComponenteBasico };
