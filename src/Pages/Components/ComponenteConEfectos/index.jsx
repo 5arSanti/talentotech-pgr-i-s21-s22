bae0f5b9b3a5ad6cd75eff1c6ffe2aed4469e6b2
5arSanti <santiari05@hotmail.com>
2024-11-24 22:30:52 -0500

    Se añadio la peticion a la API publica, y se muestra la informacion de los pokemones

--- src/Pages/Components/ComponenteConEfectos/index.jsx
+++ src/Pages/Components/ComponenteConEfectos/index.jsx
@@ -1,17 +1,36 @@
+/* eslint-disable no-unused-vars */
+
 import React from "react";
 
 const ComponenteConEfectos = () => {
     const [estadoAsincrono, setEstadoAsincrono] = React.useState("");
 
+    const [pokemones, setPokemones] = React.useState([]);
+
+    const traerPokemones = async () => {
+        try {
+            const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+            const data = await response.json();
+
+            setPokemones(data.results);
+
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
     React.useEffect(() => {
-        setTimeout(() => {
-            setEstadoAsincrono("Cambio de estado asincrono")
-        }, 5000)
+        traerPokemones();
     }, []);
 
     return(
         <div>
             <p>Valor del estado asincrono: {estadoAsincrono}</p>
+
+            <h1>Pokemones</h1>
+            {pokemones?.map((item, index) => (
+                <p key={index}>{item?.name}</p>
+            ))}
         </div>
     );
 }
